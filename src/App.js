caccb5d6f940b03be67feec6e0131df080a87d14
root <tjohnson1106@gmail.com>
2018-09-03 16:34:56 -0400

    created initial state and on change function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
 import { ChatBot } from "aws-amplify-react";
+import { Interactions } from "aws-amplify";
+import { ChatFeed, Message } from "react-chat-ui";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    input: "",
+    finalMessage: "",
+    messages: [
+      new Message({
+        id: 1,
+        message: "Hello, can I help you today?"
+      })
+    ]
+  };
+
+  onChange(e) {
+    const input = e.target.value;
+    this.setState({
+      input
+    });
+  }
+
   handleComplete(err, confirmation) {
     if (err) {
       alert("Bot conversation failed");
@@ -18,14 +37,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to
-          reload.
-        </p>
+        <ChatBot
+          title="React Bot"
+          botName=""
+          welcomeMessage="Welcome, how can I help you today?"
+          onComplete={this.handleComplete.bind(this)}
+          clearOnComplete={true}
+        />
       </div>
     );
   }
